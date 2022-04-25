010d02064c750212e575f32d38a45e6bdad4d669
Simon Herron <siherron+github@gmail.com>
2022-04-25 13:06:11 +0100

    added a demo monkey schema

--- ga.js
+++ ga.js
@@ -1,10 +1,17 @@
-function analyticsTemplate(ga, details, flagname) {
+export const analyticsTemplate = async (gtag, details, flagname) => {
 	let assignment = {
 		flagname: flagname,
 		treatment: details.variationIndex,
-		audience: details.reason.ruleid,
-		kind:details.reason.kind,
+		isExperiment: details.reason.inExperiment == null || details.reason.inExperiment == undefined ?  false : details.reason.inExperiment,
+		audience: details.reason.ruleId !== null || details.reason.ruleId !== undefined ? details.reason.ruleId: details.reason.kind,
+		audienceIndex: details.reason.ruleIndex !== null || details.reason.ruleId !== undefined ? details.reason.ruleIndex: details.reason.kind,
+		eventCreated: new Date(),
 	};
 
-if(!!window.ga) gtag("event", "feature_treatment", assignment);
+	console.log(details);
+	console.log(assignment);
+
+	if (!!window.gtag)
+		gtag("event", "feature_treatment", assignment);
+
 }
